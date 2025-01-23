cec5c29a020577a8e628755c9a9250b53c661b4f
RodrigoBCAlmeida <rodrigobomfim888@gmail.com>
2025-01-23 00:07:37 -0300

    Alteração dos inputs no client.c

--- client.c
+++ client.c
@@ -1,45 +1,57 @@
 #include "bignumber.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
-    int n;
-    scanf("%d", &n);
-    getchar();
+    char* x = NULL;
+    char* y = NULL;
+    size_t tamanhoX = 0, tamanhoY = 0;
 
-    for(int i = 0; i < n; i++){
-        char* x = NULL;
-        size_t tamanhoX = 0;
-        getline(&x, &tamanhoX, stdin);
+    while (1) {
+        if (getline(&x, &tamanhoX, stdin) == -1){
+            break;
+        }
         if (x[strlen(x) - 1] == '\n') {
             x[strlen(x) - 1] = '\0';
         }
 
-        char* y = NULL;
-        size_t tamanhoY = 0;
-        getline(&y, &tamanhoY, stdin);
+        if (getline(&y, &tamanhoY, stdin) == -1) {
+            free(x);
+            break;
+        }
         if (y[strlen(y) - 1] == '\n') {
             y[strlen(y) - 1] = '\0';
         }
 
-        BigNumber* a = string_para_BigNumber(x);
-        BigNumber* b = string_para_BigNumber(y);
+        char sinal;
+        if (scanf(" %c", &sinal) == EOF) {
+            free(x);
+            free(y);
+            break;
+        }
+        getchar(); 
+
+        BigNumber *a = string_para_BigNumber(x);
+        BigNumber *b = string_para_BigNumber(y);
 
         free(x);
         free(y);
 
-        char sinal;
-        scanf("%c", &sinal);
-        getchar();
+        BigNumber *resultado = operacao(a, b, sinal);
 
-        BigNumber* resultado = operacao(a, b, sinal);
-
-        printBigNumber(resultado);
+        if (resultado != NULL) {
+            printBigNumber(resultado);
+        }
 
         libera_BigNumber(a);
         libera_BigNumber(b);
         libera_BigNumber(resultado);
-        
+
+        x = NULL;
+        y = NULL;
+        tamanhoX = 0;
+        tamanhoY = 0;
     }
 
     return 0;
