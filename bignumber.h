488ae15ae6baba125093273ba13202a1a96d3bc0
RodrigoBCAlmeida <rodrigobomfim888@gmail.com>
2025-01-23 12:51:59 -0300

    Alteração da organização das funções e alteração de pequenos comentários.

--- bignumber.h
+++ bignumber.h
@@ -1,18 +1,18 @@
-#ifndef BIGNUMBER_H     // evita que o cabecario ser usado
+#ifndef BIGNUMBER_H     // evita que o cabeçalho seja usado
 #define BIGNUMBER_H
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
+//estutura para um nó da lista duplamente encadeada
 typedef struct Node {
     int valor;          // (0 - 9)
     struct Node* prox;  //ponteiro para o proximo numero
     struct Node* ant;   //ponteiro para o numero anterior
 }   Node;
 
-//lista duplamente encadeada
-
+//estrutura para o BigNumber
 typedef struct BigNumber {
     Node* head;         //ponteiro para início da lista
     Node* tail;         //ponteiro para final da lista
@@ -21,29 +21,27 @@ typedef struct BigNumber {
     int identificacao; //0 se é menor e 1 se é maior
 }   BigNumber;
 
-//estrutra de Bignumber, cabeça, calda, tamanho, sinal;
-
 BigNumber* criar_BigNumber();
 void adiciona(BigNumber* number, int valor);
 BigNumber* incrementa_bignumber(BigNumber* a);
 BigNumber* string_para_BigNumber(const char* string);
 void libera_BigNumber(BigNumber* number);
 void printBigNumber(BigNumber* number);
+void remove_zero(BigNumber* a);
+
+char maiorBigNumber(BigNumber* a, BigNumber* b);
+char maiorInt(int a, int b);
+
+void alteraSinalSoma(BigNumber* a, BigNumber* total);
+void alteraSinalSubtracao(BigNumber* a, BigNumber* b, BigNumber* total);
+void alteraSinalDivisaoMultiplicacao(BigNumber* a, BigNumber* b, BigNumber* total);
+
 BigNumber* soma_BigNumber(BigNumber* a, BigNumber* b);
 BigNumber* subtrai_BigNumber(BigNumber* a, BigNumber* b);
 BigNumber* multiplica_BigNumber(BigNumber* a, BigNumber* b);
 BigNumber* divide_BigNumber(BigNumber* a, BigNumber* b);
 BigNumber* resto_BigNumber(BigNumber* a, BigNumber* b);
 
-//funções
-
-char maiorBigNumber(BigNumber* a, BigNumber* b);
-char maiorInt(int a, int b);
-
 BigNumber* operacao(BigNumber* a, BigNumber* b, char sinal);
-void alteraSinalSoma(BigNumber* a, BigNumber* total);
-void alteraSinalSubtracao(BigNumber* a, BigNumber* b, BigNumber* total);
-void alteraSinalDivisaoMultiplicacao(BigNumber* a, BigNumber* b, BigNumber* total);
-void remove_zero(BigNumber* a);
 
 #endif //BIGNUMBER_H
